f4326fd21532d78314f4a06b61d59bc78062a556
CeCHEN <cc310@students.waikato.ac.nz>
2021-04-28 15:23:50 +1200

    v0.0 ~ Project initiation.

---
+++ src/test/java/com/erqs/examrecordquerysystem/ExamRecordQuerySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erqs.examrecordquerysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamRecordQuerySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
